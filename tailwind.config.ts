066fbf49e0d6d58593f022534fca2effbf47bc69
dongeul24 <gogo0232@naver.com>
2025-01-06 20:40:42 +0900

    style: 반응형 스크린 사이즈, 최대너비 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
-    },
+      screens: {
+        mobile: '320px', // => @media (min-width: 320px) { ... }
+        tablet: '768px', // => @media (min-width: 768px) { ... }
+        desktop: '1025px' // => @media (min-width: 1025px) { ... }
+      },
+      maxWidth: {
+        content: '1440px' //"max-w-content
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
