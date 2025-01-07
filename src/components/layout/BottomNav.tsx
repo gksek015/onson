dee1882ee6767962807736461158385ecf9a0af4
leekyoungmin <zzxx66052@gmail.com>
2025-01-07 15:40:23 +0900

    feat: layout 헤더 layout 1차 제작완료

--- src/components/layout/BottomNav.tsx
+++ src/components/layout/BottomNav.tsx
@@ -1,22 +1,31 @@
+import Link from 'next/link';
+
 const BottomNav = () => {
   return (
-    <nav className="fixed bottom-0 w-full bg-gray-100 border-t p-3 flex justify-around">
-      <button className="flex flex-col items-center text-gray-600 hover:text-blue-600">
+    <nav className="fixed bottom-0 w-full bg-gray-100 border-t p-4 flex justify-around">
+      {/* 홈 버튼 */}
+      <Link href="/" className="flex flex-col items-center hover:text-blue-600">
         <span>🏠</span>
-        <span className="text-xs">홈</span>
-      </button>
-      <button className="flex flex-col items-center text-gray-600 hover:text-blue-600">
+        <span className="text-sm text-black font-bold">홈</span>
+      </Link>
+
+      {/* 게시글 작성 버튼 */}
+      <Link href="/create" className="flex flex-col items-center  hover:text-blue-600">
         <span>📄</span>
-        <span className="text-xs">봉사 요청</span>
-      </button>
-      <button className="flex flex-col items-center text-gray-600 hover:text-blue-600">
+        <span className="text-sm text-black font-bold">봉사 요청</span>
+      </Link>
+
+      {/* 게시글 리스트 페이지 이동 버튼 */}
+      <Link href="search-list" className="flex flex-col items-center hover:text-blue-600">
         <span>🔍</span>
-        <span className="text-xs">봉사 찾기</span>
-      </button>
-      <button className="flex flex-col items-center text-gray-600 hover:text-blue-600">
+        <span className="text-sm text-black font-bold">봉사 찾기</span>
+      </Link>
+
+      {/* 마이페이지 */}
+      <Link href="my-page" className="flex flex-col items-center hover:text-blue-600">
         <span>👤</span>
-        <span className="text-xs">마이페이지</span>
-      </button>
+        <span className="text-sm text-black font-bold">마이페이지</span>
+      </Link>
     </nav>
   );
 };
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,11 @@
+import ProfileDropdawn from './ProfileDropdown';
+
 const Header = () => {
   return (
     <div>
       <header className="bg-white shadow-md p-4 flex justify-between items-center">
         <div className="text-xl font-bold text-black">ON:SON</div>
-        <div className="relative w-10 h-10">
-          <img
-            src="https://via.placeholder.com/40"
-            alt="Profile"
-            className="w-10 h-10 rounded-full object-cover border border-gray-300"
-          />
-        </div>
+        <ProfileDropdawn />
       </header>
     </div>
   );
