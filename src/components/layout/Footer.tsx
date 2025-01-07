73a523149d434e37bbdb43f275f969c8e2c6dad2
leekyoungmin <zzxx66052@gmail.com>
2025-01-08 00:37:51 +0900

    feat: 드롭다운 부분 수정 및 모바일 디자인 재구성

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,43 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-white py-8 border-t hidden sm:block">
+      {/* 푸터 컨텐츠 */}
+      <div className="max-w-content mx-auto flex justify-between text-sm text-gray-600 min-h-[100px]">
+        {/* 왼쪽 로고 */}
+        <div className="text-xl font-bold text-black">ON:SON</div>
 
-export default Footer
+        {/* 중앙 정보 */}
+        <div className="flex flex-row">
+          <div className="flex flex-col space-y-2 mr-12">
+            <div>
+              <span className="font-semibold">대표진</span> 김진실, 김문식, 서한솔, 이경민, 한다영
+            </div>
+            <div>
+              <span className="font-semibold">주소</span> 서울특별시 강남구 테헤란로 44길 8 12층
+            </div>
+            <div>
+              <span className="font-semibold">사업자등록번호</span> 783-86-01715
+            </div>
+          </div>
+
+          {/* 오른쪽 정보 */}
+          <div className="flex flex-col space-y-2 text-right">
+            <div>
+              <span className="font-semibold">EMAIL</span> 1j5p@onson.dev
+            </div>
+            <div>
+              <span className="font-semibold">TEL</span> 02-1522-8016
+            </div>
+            <div>
+              <span className="font-semibold">FAX</span> 02-1522-8016
+            </div>
+
+            <div className="text-gray-500 text-xs mt-4">© 2025 ONSON All Right Reserved.</div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/layout/header-right/RightSide.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import ProfileDropdawn from './ProfileDropdown';
+
+const RightSide = () => {
+  return (
+    <div className="flex flex-row gap-4">
+      <Link
+        href="/login"
+        className="text-black  
+      border border-black rounded-full px-4 py-2 hover:bg-blue-200"
+      >
+        봉사요청
+      </Link>
+
+      <Link
+        href="/search-list"
+        className="text-black  
+      border border-black rounded-full px-4 py-2 hover:bg-blue-200"
+      >
+        봉사찾기
+      </Link>
+
+      <ProfileDropdawn />
+    </div>
+  );
+};
+
+export default RightSide;
