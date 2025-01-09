30ae796f3c90758260d16f13d6c12371163c453d
jinsil-kim <wlstlf9315@gmail.com>
2025-01-09 19:14:26 +0900

    feat: 게시글 리스트 완성 및 id받아서 디테일 페이지로 넘어가게 구현 완료

--- src/components/home/VolunteerCard.tsx
+++ src/components/home/VolunteerCard.tsx
@@ -1,29 +1,51 @@
-import Image from 'next/image';
-import React from 'react';
-import oldman2 from '@/assets/oldman2.jpg';
+'use client';
 
-const VolunteerCard = () => {
+import Image from 'next/image';
+// import React, { useEffect } from 'react';
+// import oldman2 from '@/assets/oldman2.jpg';
+import { PostType } from '@/api/getPosts';
+import Link from 'next/link';
+
+interface VolunteerCardProps {
+  post: PostType;
+}
+
+const VolunteerCard = ({ post }: VolunteerCardProps) => {
+
+
+  // console.log(post);
   return (
-    <div className="flex items-center pt-3">
-      <div className="max-w-content border mx-auto rounded-lg">
-        <div className="flex flex-col md:flex-row gap-12">
-          <div className="p-5">
-            <div className="flex items-center gap-2 mb-2">
-              <span className="px-3 py-1 bg-gray-400 text-white text-xs rounded-full">유기견 봉사</span>
-              <span className="px-3 py-1 bg-gray-400 text-white text-xs rounded-full">25.01.02~25.02.09</span>
-            </div>
-            <h3 className="text-lg font-semibold mb-1">[인천광역시 남동구] 연탄 봉사, 연초 정기 봉사자 구합니다</h3>
-            <div className="flex items-center text-sm text-gray-600">
-              <span className="mr-4">이지원</span>
-              <span>25.01.02</span>
-            </div>
-            <div className="md:w-1/2 flex flex-col items-center w-64 mt-6">
-              <Image src={oldman2} alt='photo' className="object-cover" />
+    <Link href={`/detail/${post.id}`}>
+      <div className="flex items-center pt-3">
+        <div className="max-w-content border mx-auto rounded-lg">
+          <div className="flex flex-col md:flex-row gap-12">
+            <div className="p-5">
+              <div className="flex items-center gap-2 mb-2">
+                <span className="px-3 py-1 bg-gray-400 text-white text-xs rounded-full">{post.category}</span>
+                <span className="px-3 py-1 bg-gray-400 text-white text-xs rounded-full">{post.date}</span>
+              </div>
+              <h3 className="text-lg font-semibold mb-1">{post.title}</h3>
+              <div className="flex items-center text-sm text-gray-600">
+                <span className="mr-4">{post.users.nickname}</span>
+                <span>{post.created_at.split('T')[0]}</span>
+              </div>
+              <div className="md:w-1/2 flex flex-col items-center w-64 mt-6">
+                {post.images?.map((image, index) => (
+                  <Image
+                    key={index}
+                    src={image?.img_url}
+                    alt={post.title}
+                    width={800}
+                    height={500}
+                    className="object-cover"
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
