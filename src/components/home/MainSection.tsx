1b74d3e53bf5f7c231333f572fa806dad0fe1d3f
jinsil-kim <wlstlf9315@gmail.com>
2025-01-13 22:43:07 +0900

    refactor: 최신글 8개 보이기 코드 리팩토링

--- src/components/home/MainSection.tsx
+++ src/components/home/MainSection.tsx
@@ -1,23 +1,23 @@
 'use client';
 
-import useGetPostMain from '@/app/hooks/useGetPostMain';
-import CategoryButton from '@/components/home/CategoryButton';
+import useGetPost from '@/app/hooks/useGetPost';
+import AddressButton from '@/components/home/AddressButton';
 import HeroSection from '@/components/home/HeroSection';
-import SearchBar from '@/components/home/SearchBar';
 import VolunteerCard from './VolunteerCard';
 
 const MainSection = () => {
-  const { posts, loading } = useGetPostMain();
+  const { posts, isPending } = useGetPost();
 
-  if (loading) {
+  if (isPending) {
     return <div>Loading...</div>;
   }
+
+  const recentPosts = posts?.slice(0, 8);
 
   return (
     <section>
       <div className="mx-4 flex items-center justify-center gap-3">
-        <CategoryButton />
-        <SearchBar />
+        <AddressButton />
       </div>
       <div>
         <HeroSection />
@@ -25,9 +25,7 @@ const MainSection = () => {
       <div className="py-4">
         <h2 className="px-5 text-xl font-semibold">방금 등록된 봉사</h2>
         <div className="grid grid-cols-1 gap-4 px-5 pb-20 sm:grid-cols-2 lg:grid-cols-4">
-          {posts.map((post) => (
-            <VolunteerCard key={post.id} post={post} />
-          ))}
+          {recentPosts?.map((post) => <VolunteerCard key={post.id} post={post} />)}
         </div>
       </div>
     </section>
