0bcd8489db9a79782b8fd14cd2c32ca4c772902a
jinsil-kim <wlstlf9315@gmail.com>
2025-01-14 16:19:24 +0900

    chore: list 페이지 헤더 추가

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,11 +1,15 @@
 import SearchBar from '@/components/home/SearchBar';
+import Header from '@/components/layout/Header';
 import AllLists from './_components/AllLists';
 
 const ListPage = () => {
   return (
-    <div className="mx-4 flex flex-col items-center justify-center gap-3">
-      <SearchBar />
-      <AllLists />
+    <div>
+      <Header />
+      <div className="mt-5 mb-20 flex flex-col items-center justify-center gap-3">
+        <SearchBar />
+        <AllLists />
+      </div>
     </div>
   );
 };
