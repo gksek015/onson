02bd18d5b1bc61788f25526e9d811a7be19839d2
jinsil-kim <wlstlf9315@gmail.com>
2025-01-16 11:29:06 +0900

    fix: useSearchParams 사용으로 suspense 처리해줌

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,21 +1,21 @@
 import CategoryButton from '@/app/list/_components/CategoryButton';
 import Tags from '@/app/list/_components/Tags';
 import Header from '@/components/layout/Header';
+import { categories } from '@/data/categories';
 import { Suspense } from 'react';
 import AllLists from './_components/AllLists';
 import SearchBar from './_components/SearchBar';
-import { categories } from '@/data/categories';
 
 const ListPage = () => {
   return (
     <div>
       <Header />
       <div className="mb-20 mt-5 flex w-full flex-col items-center justify-center gap-3">
-        <div className="flex w-full items-center justify-center gap-4">
-          <SearchBar />
-          <CategoryButton categories={categories} />
-        </div>
         <Suspense>
+          <div className="flex w-full items-center justify-center gap-4">
+            <SearchBar />
+            <CategoryButton categories={categories} />
+          </div>
           <Tags />
           <AllLists />
         </Suspense>
