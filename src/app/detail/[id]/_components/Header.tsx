72a36dc53feabe0a19a6f7511c08f41439be40f0
dongeul24 <gogo0232@naver.com>
2025-01-16 21:00:22 +0900

    feat: 모바일 바텀 시트 구현 완료, 채팅 기능을 위한 리펙토링 완료

--- src/app/detail/[id]/_components/Header.tsx
+++ src/app/detail/[id]/_components/Header.tsx
@@ -1,24 +1,80 @@
 'use client';
 
+import { BackButtonIcon, MeatballMenuIcon, PencilIcon, TrashBinIcon } from '@/components/icons/Icons';
+import useGetPostById from '@/hooks/useGetPostById';
+import { useUserStore } from '@/utils/store/userStore';
 import { useRouter } from 'next/navigation';
-import { BackButtonIcon, ShareIcon } from '@/components/icons/Icons';
+import { useState } from 'react';
+import { BottomSheet } from 'react-spring-bottom-sheet-updated';
+import 'react-spring-bottom-sheet-updated/dist/style.css';
 
+interface PostDetailProps {
+  postPageId: string;
+}
 
-// 상세글 헤더 부분
-const Header = () => {
+const Header = ({ postPageId }: PostDetailProps) => {
+  const { data: post } = useGetPostById(postPageId);
+  const { user } = useUserStore();
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
   const router = useRouter();
+
   const handleBack = () => {
-    router.push('/list'); 
+    router.push('/list');
+  };
+
+  const openSheet = () => setIsSheetOpen(true);
+  const closeSheet = () => setIsSheetOpen(false);
+
+  const handleEdit = () => {
+    router.push(`/post-update/${postPageId}`);
+  };
+
+  const handleDelete = () => {
+    // TODO: delete 기능 구현
+    console.log('게시물 삭제');
+    closeSheet();
+  };
+
+  const handleToggleRecruitment = () => {
+    // TODO: 모집중, 완료 기능 구현
+    console.log('모집 상태 변경');
+    closeSheet();
   };
 
   return (
     <div className="flex items-center justify-between border-b px-2 py-2">
       <button onClick={handleBack}>
-        <BackButtonIcon/>
+        <BackButtonIcon />
       </button>
-      <button>
-        <ShareIcon/>
-      </button>
+
+      {/* 수정 삭제 버튼은 로그인 유저가 포스트 유저와 같은 id일때만 보여줌. */}
+      {user?.id === post?.user_id && (
+        <button onClick={openSheet}>
+          <MeatballMenuIcon />
+        </button>
+      )}
+
+      {/* 바텀 시트*/}
+      <BottomSheet
+        open={isSheetOpen}
+        onDismiss={closeSheet}
+        snapPoints={({ maxHeight }) => [maxHeight * 0.3, maxHeight * 0.4]} // 스냅 포인트 설정(참고로 첫번째는 처음 열리는 위치, 두번쨰는 사용자가 올릴 수 있는 만큼 올린 위치임)
+        defaultSnap={({ maxHeight }) => maxHeight * 0.3} // 기본 열림 위치 설정
+      >
+        <div className="p-4">
+          <button onClick={handleEdit} className="flex w-full items-center gap-2 px-4 py-2 text-left">
+            <PencilIcon />
+            게시물 수정하기
+          </button>
+          <button onClick={handleDelete} className="flex w-full items-center gap-3 px-4 py-2 text-left text-red-500">
+            <TrashBinIcon />
+            게시물 삭제하기
+          </button>
+          <button onClick={handleToggleRecruitment} className="flex w-full items-center gap-2 px-4 py-2 text-left">
+            모집 마감하기
+          </button>
+        </div>
+      </BottomSheet>
     </div>
   );
 };
