8765300c524621e742ed85a8959536245ef13320
jinsil-kim <wlstlf9315@gmail.com>
2025-01-18 00:31:53 +0900

    fix : 상세게시글 부분 북마크 아이콘, 날짜 수정

--- src/app/detail/[id]/_components/BookmarkButton.tsx
+++ src/app/detail/[id]/_components/BookmarkButton.tsx
@@ -42,12 +42,8 @@ const BookmarkButton = ({ postId }: BookmarkButtonProps) => {
   };
 
   return (
-    <div
-      className="flex cursor-pointer items-center rounded-lg bg-slate-100 px-3 py-2 text-gray-600"
-      onClick={handleBookmark}
-    >
+    <div className="flex cursor-pointer" onClick={handleBookmark}>
       {isBookmarked ? <BookmarkColorIcon /> : <BookmarkBlackIcon />}
-      <span className="ml-1 text-sm">{isBookmarked ? '저장됨' : '저장'}</span>
     </div>
   );
 };
--- src/app/detail/[id]/_components/PostTags.tsx
+++ src/app/detail/[id]/_components/PostTags.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import dayjs from "dayjs";
+
 interface PostTagsProps {
   category: string;
   startDate: string;
@@ -8,6 +10,11 @@ interface PostTagsProps {
 }
 
 const PostTags = ({ category, startDate, endDate, isPostClosed }: PostTagsProps) => {
+
+    const formattedStart = dayjs(startDate).format('YY.MM.DD.');
+    const formattedEnd = dayjs(endDate).format('YY.MM.DD.');
+  
+  
   return (
     <div className="flex flex-wrap items-center gap-2 text-sm font-normal text-gray-500">
       {/* 모집중/ 마감 태그 */}
@@ -17,7 +24,7 @@ const PostTags = ({ category, startDate, endDate, isPostClosed }: PostTagsProps)
 
       {/* 날짜(기간) 태그 */}
       <span className="rounded-full border-2 border-gray-200 px-3 py-1 text-gray-600">
-        {startDate} ~ {endDate}
+        {formattedStart} ~ {formattedEnd}
       </span>
 
       {/* 카테고리 태그 */}
