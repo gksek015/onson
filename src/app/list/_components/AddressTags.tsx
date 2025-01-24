2744cb3516e37fae613936c8921875af15429b1d
jinsil-kim <wlstlf9315@gmail.com>
2025-01-24 14:11:19 +0900

    feat: 지역태그 닫기 버튼 누르면 사라지지 않고 전체 버튼으로 바뀌도록 기능 추가

--- src/app/list/_components/AddressTags.tsx
+++ src/app/list/_components/AddressTags.tsx
@@ -8,20 +8,28 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
 const AddressTags = () => {
-      const { open, close } = useBottomSheetStore();
+  const { open, close } = useBottomSheetStore();
   const searchParams = useSearchParams();
   const router = useRouter();
   const [tag, setTag] = useState(searchParams.get('addressKeyword'));
 
-  // 태그 지우기
-  const removeAddress = () => {
+  // // 태그 지우기
+  // const removeAddress = () => {
+  //   const params = new URLSearchParams(searchParams.toString());
+  //   params.delete('addressKeyword');
+  //   params.delete('address');
+  //   router.push(`?${params.toString()}`);
+  //   setTag(null);
+  // };
+
+  // 태그 초기화 (전체로 전환)
+  const resetToAll = () => {
     const params = new URLSearchParams(searchParams.toString());
     params.delete('addressKeyword');
     params.delete('address');
     router.push(`?${params.toString()}`);
-    setTag(null);
+    setTag('전체');
   };
-
 
   const handleAddressSelect = (searchKeyword: string) => {
     setTag(searchKeyword);
@@ -34,24 +42,27 @@ const AddressTags = () => {
       {tag && (
         <div
           onClick={() => open('sheetD')}
-          className="flex cursor-pointer rounded-lg border  border-[#e6e6e6] py-1.5 pl-2 pr-1.5"
+          className="flex cursor-pointer rounded-lg border border-[#e6e6e6] py-1.5 pl-2 pr-1.5"
         >
           <MapPinIcon />
           <span className="text-sm text-[#656565]">{tag}</span>
-          <button
-            className="ml-3"
-            onClick={(e) => {
-              e.stopPropagation();
-              removeAddress();
-            }}
-          >
-            <CloseIcon width="14px" height="14px" />
-          </button>
+
+          {tag !== '전체' && ( // 태그가 '전체'가 아닐 때만 닫기 버튼 보여줌
+            <button
+              className="ml-3"
+              onClick={(e) => {
+                e.stopPropagation(); // 이벤트 전파 중단
+                resetToAll(); // 태그를 '전체'로 변경
+              }}
+            >
+              <CloseIcon width="14px" height="14px" />
+            </button>
+          )}
         </div>
       )}
 
       {/* BottomSheet */}
-      <BottomSheet id='sheetD'>
+      <BottomSheet id="sheetD">
         {/* AddressSearch를 바텀시트에 렌더링 */}
         <AddressSearch option={'search'} onAddressSelect={handleAddressSelect} />
       </BottomSheet>
